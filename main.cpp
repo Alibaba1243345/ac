ce022cf87f245382fa7bc29c6a689c8768a86c5c
Nikita Sulima <nikvip5000@gmail.com>
2022-01-14 10:48:02 +0700

    good

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QCoreApplication>
+#include <iostream>
 
 struct Date{
     int day;
@@ -9,6 +10,10 @@ struct Date{
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    Date x;
+    x.day = 14;
+    x.month = 1;
+    x.yare = 2022;
+    std::cout << x.day << "." << x.month << "." << x.year <<std::endl;
     return a.exec();
 }
